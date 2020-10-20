b9f2d0d5d6c3dbc4dcec37a312b016306fa875de
zoelzer220 <mmp6301@gmail.com>
2020-10-19 23:31:32 -0500

    Final assignment, no bonus missions.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,6 +23,27 @@ public class SearchController {
         return "search";
     }
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs With " + searchType + ": " + searchTerm);
+        } else if (searchType.toLowerCase().equals("all")){
+                jobs = JobData.findByValue(searchTerm);
+                model.addAttribute("title", "Jobs With All: " + searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs With " + searchType + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
+
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
